5c2c268e1e5d5cb47af916f2411318047cd5e1d1
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2020-12-09 15:40:15 +0100

    Animation testing Everything successfully tested and working as intended.

--- src/components/animated/FadeIn.tsx
+++ src/components/animated/FadeIn.tsx
@@ -1,22 +1,36 @@
 import React, { useRef } from 'react';
 import { CSSTransition } from 'react-transition-group';
 
+import { fadeInDuration } from '../../constants/durations';
+
 interface FadeInProp {
   appearCondition?: boolean;
+  appearDelayMultiplier?: number;
 }
 
-const FadeIn: React.FC<FadeInProp> = ({ children, appearCondition }) => {
+const FadeIn: React.FC<FadeInProp> = ({
+  children,
+  appearCondition,
+  appearDelayMultiplier,
+}) => {
   const nodeRef = useRef(null);
 
   return (
     <CSSTransition
-      in={typeof appearCondition === 'boolean' ? appearCondition : true}
+      in={typeof appearCondition !== 'undefined' ? appearCondition : true}
       appear
       enter
       mountOnEnter
       unmountOnExit
       classNames='FadeIn'
-      timeout={1000}
+      timeout={{
+        appear:
+          typeof appearDelayMultiplier !== 'undefined'
+            ? fadeInDuration * appearDelayMultiplier
+            : fadeInDuration,
+        enter: fadeInDuration,
+        exit: fadeInDuration,
+      }}
       nodeRef={nodeRef}
     >
       <div className='FadeIn' ref={nodeRef}>
