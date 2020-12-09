726ac0a17d2e0c68963c5d90e20d62a8c00fce40
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2020-12-09 08:40:07 +0100

    Cleanup Removed some starting files and set up default style files and components.

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Nav from './Nav';
+
+const App: React.FC = () => {
+  return (
+    <div className='App'>
+      <Nav />
+    </div>
+  );
+};
+
+export default App;
