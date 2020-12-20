66099f9522053cee93c62cb40c2f9c96fdd501b7
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2020-12-20 10:05:47 +0100

    Pagination for makes Functional, albeit style-less, pagination for makes on Dashboard page for admins.

--- src/components/structural/MakesList.tsx
+++ src/components/structural/MakesList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { observer } from 'mobx-react';
 
 import { VehicleMake } from '../../mobx/stores/vehiclesStore';
@@ -8,12 +8,38 @@ interface MakesListProps {
 }
 
 const MakesList: React.FC<MakesListProps> = ({ makes }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+
+  const generatePageNumbers = () => {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= Math.ceil(makes.length) / itemsPerPage; i++) {
+      pageNumbers.push(i);
+    }
+
+    return pageNumbers;
+  };
+
   return (
     <div className='MakesList'>
       <h1>MakesList</h1>
-      {makes.map((make) => (
-        <li key={make.ID}>{make.name}</li>
+
+      {generatePageNumbers().map((page) => (
+        <button
+          disabled={currentPage === page}
+          key={page}
+          onClick={() => setCurrentPage(page)}
+        >
+          {page}
+        </button>
       ))}
+
+      {makes
+        .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
+        .map((make) => (
+          <li key={make.ID}>{make.name}</li>
+        ))}
     </div>
   );
 };
