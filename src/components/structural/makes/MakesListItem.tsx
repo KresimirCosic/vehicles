2a0e4b3a0c3985cd68ac7db4bc6c4b6ffa1c167a
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2020-12-22 11:09:39 +0100

    Makes styling and total cars Started to style the components for makes and to finish off with CRUD operations and the their respected forms.

--- src/components/structural/makes/MakesListItem.tsx
+++ src/components/structural/makes/MakesListItem.tsx
@@ -3,6 +3,7 @@ import { CSSTransition } from 'react-transition-group';
 
 import { VehicleMake } from '../../../mobx/stores/vehiclesStore';
 import { fadeInDuration } from '../../../constants/durations';
+import { useRootStore } from '../../../mobx/hooks/useRootStore';
 
 interface MakesListItemProps {
   index: number;
@@ -11,6 +12,16 @@ interface MakesListItemProps {
 
 const MakesListItem: React.FC<MakesListItemProps> = ({ index, make }) => {
   const nodeRef = useRef(null);
+  const { vehiclesStore } = useRootStore();
+
+  const numberOfVehicles = () => {
+    return vehiclesStore.models.reduce((totalNumber, model) => {
+      if (make.ID === model.makeID) {
+        return totalNumber + 1;
+      }
+      return totalNumber;
+    }, 0);
+  };
 
   return (
     <CSSTransition
@@ -28,7 +39,20 @@ const MakesListItem: React.FC<MakesListItemProps> = ({ index, make }) => {
       nodeRef={nodeRef}
     >
       <li className='MakesListItem' ref={nodeRef}>
-        {make.name}
+        <div className='MakesListItem-card'>
+          <h4 className='MakesListItem-card-name'>{make.name}</h4>
+          <h6 className='MakesListItem-card-number'>
+            {numberOfVehicles()} models in store
+          </h6>
+        </div>
+        <div className='MakesListItem-controls'>
+          <button className='MakesListItem-controls-button'>
+            Edit the make
+          </button>
+          <button className='MakesListItem-controls-button'>
+            Add a new model
+          </button>
+        </div>
       </li>
     </CSSTransition>
   );
